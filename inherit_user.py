288e74b38167220a4b2390267873d8bcd17c1b27
Safwene Ladhari <axelsaff@gmail.com>
2016-08-10 09:03:40 +0100

    Revert "Create inherit_user.py"

--- inherit_user.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import fields, models
-
-class display_notif (models.Model):
-    			
-    _inherit =['res.users']
-    notif_user = fields.Boolean("Enable Push Notification", default=False)
